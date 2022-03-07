530b16c4637da700beda9ed0271bcd6041cfa639
Bernard Halas <bernard.halas@berops.com>
2022-03-07 12:17:20 +0100

    Move all deployments into deploy/
    This is a code re-org.

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-ytt -f ns.yaml \
-    -f labels.lib.yaml \
-    -f metadata.lib.yaml \
-    -f data.yaml \
-    | kubectl apply -f - --server-side
-
-ytt -f alertmanager \
-    -f blackboxExporter \
-    -f grafana \
-    -f kubePrometheus \
-    -f promAdapter \
-    -f prometheus \
-    -f labels.lib.yaml \
-    -f metadata.lib.yaml \
-    -f data.yaml \
+ytt -f examples/data \
+    -f deploy \
     | kubectl apply -f - --server-side
